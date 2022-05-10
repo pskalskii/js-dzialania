43b59568661886d305f33584cbf42f8868f0296c
pskalskii <pawelskalski97@gmail.com>
2022-05-10 23:03:53 +0200

    poprawione zadanie

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-function fewOperations(num1,num2){
-    if (num1+num2>=0){
-        console.log("Wynik dodawania wynosi"+(num1+num2));
+function fewOperations(num1, num2) {
+    if (num1 + num2 >= 0) {
+        console.log("Wynik dodawania wynosi" + (num1 + num2));
     }
     else {
         console.log("Wynik działania jest nieprawidłowy");
     }
-    if (num1-num2>=0){
-        console.log("Wynik odejmowania wynosi"+(num1-num2));
+    if (num1 - num2 >= 0) {
+        console.log("Wynik odejmowania wynosi" + (num1 - num2));
     }
     else {
         console.log("Wynik działania jest nieprawidłowy");
     }
-    if (num1*num2>=0){
-        console.log("Wynik mnożenia wynosi"+(num1*num2));
+    if (num1 * num2 >= 0) {
+        console.log("Wynik mnożenia wynosi" + (num1 * num2));
     }
     else {
         console.log("Wynik działania jest nieprawidłowy");
@@ -22,7 +22,7 @@ function fewOperations(num1,num2){
 
 }
 
-console.log(fewOperations(20,14));
-console.log(fewOperations(-30,24));
-console.log(fewOperations(45,55));
-console.log(fewOperations(-76,14));
+    fewOperations(20, 14);
+    fewOperations(-30, 24);
+    fewOperations(45, 55);
+    fewOperations(-76, 14);
